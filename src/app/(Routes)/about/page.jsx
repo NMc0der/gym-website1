d02b935607019800c820d540d359886af4665fd4
Noah <noahglennmaldonado@gmail.com>
2024-10-21 13:01:45 -0400

    created ceo headshot image

--- src/app/(Routes)/about/page.jsx
+++ src/app/(Routes)/about/page.jsx
@@ -1,6 +1,14 @@
 import "./about.scss";
-
+import Image from "next/image";
+import ceo from "../../assets/lance-reis-pp76Y6Fq6xw-unsplash.jpg";
 const About = () => {
-  return <div className="hi">hi</div>;
+  return (
+    <div className="hi">
+      <div className="ceo-image-container">
+        <Image className="ceo-image" src={ceo} />
+      </div>
+      <div className="ceo-info-container"></div>
+    </div>
+  );
 };
 export default About;
