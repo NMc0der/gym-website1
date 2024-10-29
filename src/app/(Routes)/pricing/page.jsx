772096bca6e7d6e3c37fcd92e76937306acf63fd
Noah <noahglennmaldonado@gmail.com>
2024-10-29 05:59:38 -0400

    basic template for plan template complete

--- src/app/(Routes)/pricing/page.jsx
+++ src/app/(Routes)/pricing/page.jsx
@@ -1,10 +1,32 @@
 import "./pricing.scss";
+import { bebas_neue } from "@/app/fonts/googleFonts";
+import img1 from "../../assets/alexander-redl-d3bYmnZ0ank-unsplash.jpg";
+import PlanTemaplate from "@/app/components/planTemplate";
 
 const Pricing = () => {
   return (
     <>
-      <div className="pricing-text-container"></div>
-      <div className="pricing-plan-container"></div>
+      <div className="pricing-text-container">
+        <h2 className={`${bebas_neue.className}`}>Pricing Plan</h2>
+        <p>
+          Sign up today with a fitness plan that works for both your schedule
+          and fitness level.
+        </p>
+      </div>
+      <div className="pricing-plan-container">
+        <PlanTemaplate
+          image={img1}
+          name={"Beginners"}
+          price={"39"}
+          classes={[
+            "Free Hand",
+            "Gym Fitness",
+            "Weight Loss",
+            "Yoga",
+            "Cardio",
+          ]}
+        />
+      </div>
     </>
   );
 };
---
+++ src/app/components/planTemplate.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Button from "./button";
+
+const PlanTemaplate = ({ image, name, price, classes }) => {
+  return (
+    <div className="template-container">
+      <div className="template-image-container">
+        <Image src={image} />
+      </div>
+      <div className="template-info-container">
+        <h3 className="template-name">{name}</h3>
+        <div className="template-price-container">
+          <span className="template-dollar-sign">$</span>
+          <span className="template-price">{price}</span>
+          <span className="template-pm">p/m</span>
+        </div>
+        <div className="class-names-data">
+          {classes.map((gymClass) => {
+            return <p>{gymClass}</p>;
+          })}
+        </div>
+        <Button />
+      </div>
+    </div>
+  );
+};
+
+export default PlanTemaplate;
