c8e9f549bd823d8767c74f5d56ea69b27ea4ad4a
Noah <noahglennmaldonado@gmail.com>
2024-11-07 07:22:29 -0500

    made gym classes page responsive

--- src/app/(Routes)/gymClasses/page.jsx
+++ src/app/(Routes)/gymClasses/page.jsx
@@ -5,19 +5,21 @@ import GymClassList from "@/app/data/gymClassList";
 
 const GymClasses = () => {
   return (
-    <div className="gym-classes-container">
-      {GymClassList.map((gymC) => {
-        return (
-          <GymClassCard
-            key={gymC.name}
-            gymClass={gymC.name}
-            teacher={gymC.teacher}
-            day={gymC.day}
-            time={gymC.time}
-            image={gymC.image}
-          />
-        );
-      })}
+    <div className="classes-page-container">
+      <div className="gym-classes-container">
+        {GymClassList.map((gymC) => {
+          return (
+            <GymClassCard
+              key={gymC.name}
+              gymClass={gymC.name}
+              teacher={gymC.teacher}
+              day={gymC.day}
+              time={gymC.time}
+              image={gymC.image}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/app/data/gymClassList.jsx
+++ src/app/data/gymClassList.jsx
@@ -1,6 +1,7 @@
 import workoutPic1 from "../assets/victor-freitas-vqDAUejnwKw-unsplash.jpg";
 import running from "../assets/bradley-dunn-fjpl1yrNvNQ-unsplash.jpg";
 import yoga from "../assets/charlotte-karlsen-awyTiNB8tKU-unsplash.jpg";
+import powerlifting from "../assets/nathan-dumlao-NXMZxygMw8o-unsplash.jpg";
 
 const GymClassList = [
   {
@@ -14,8 +15,15 @@ const GymClassList = [
     name: "Yoga",
     image: yoga,
     teacher: "Emily Jacobs",
-    day: "Thu",
-    time: "7:00 pm",
+    day: " Thu",
+    time: " 7:00 pm",
+  },
+  {
+    name: "Power Lifting",
+    image: powerlifting,
+    teacher: "Cody Wyane",
+    day: " Tue",
+    time: " 6:00 pm",
   },
 ];
 
