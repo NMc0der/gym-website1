dfa451d0683f9227983712f638ff3a1eaa1f7c23
Noah <noahglennmaldonado@gmail.com>
2024-11-07 08:34:16 -0500

    added more entries to classes list

--- src/app/data/gymClassList.jsx
+++ src/app/data/gymClassList.jsx
@@ -2,6 +2,13 @@ import workoutPic1 from "../assets/victor-freitas-vqDAUejnwKw-unsplash.jpg";
 import running from "../assets/bradley-dunn-fjpl1yrNvNQ-unsplash.jpg";
 import yoga from "../assets/charlotte-karlsen-awyTiNB8tKU-unsplash.jpg";
 import powerlifting from "../assets/nathan-dumlao-NXMZxygMw8o-unsplash.jpg";
+import boxing from "../assets/eser-goat-QPd3g4xFfGc-unsplash.jpg";
+import cycling from "../assets/marc-antoine-dube-C1A8Yk8DaSM-unsplash.jpg";
+import crossfit from "../assets/mads-eneqvist-c-iX42QHg3w-unsplash.jpg";
+import pilates from "../assets/logan-weaver-lgnwvr-u76Gd0hP5w4-unsplash.jpg";
+import fullBody from "../assets/mediamodifier-m6Hw3FybWPA-unsplash.jpg";
+import jumpRope from "../assets/charlotte-karlsen-Pz2Yhpx2ZdQ-unsplash.jpg";
+import meditation from "../assets/zen-bear-yoga-V6Ct9yq17hI-unsplash.jpg";
 
 const GymClassList = [
   {
@@ -24,6 +31,55 @@ const GymClassList = [
     teacher: "Cody Wyane",
     day: " Tue",
     time: " 6:00 pm",
+  },
+  {
+    name: "Boxing",
+    image: boxing,
+    teacher: "Mike Alex",
+    day: " Mon",
+    time: " 7:00pm",
+  },
+  {
+    name: "Cycling",
+    image: cycling,
+    teacher: "Cloe Weaver",
+    day: " Tue",
+    time: " 7:00 pm",
+  },
+  {
+    name: "Crossfit",
+    image: crossfit,
+    teacher: "Shawn Richard",
+    day: " Wed",
+    time: " 5:00 am",
+  },
+  {
+    name: "Pilates",
+    image: pilates,
+    teacher: "Olivia Miler",
+    day: " Wed",
+    time: " 7:00 pm",
+  },
+  {
+    name: "Full-body",
+    image: fullBody,
+    teacher: "Tyler Wilson",
+    day: "Mon",
+    time: "7:30 pm",
+  },
+  {
+    name: "Jump Rope",
+    image: jumpRope,
+    teacher: "Kyle Jackson",
+    day: " Tue",
+    time: " 5:00 am",
+  },
+  {
+    name: "Meditation",
+    image: meditation,
+    teacher: "Luna Taylor",
+    day: " Wed",
+    time: "6:00 am",
   },
 ];
 
