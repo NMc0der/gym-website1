9dbec919e72f6083b3dd82dfe7b1df2374f854fe
Noah <noahglennmaldonado@gmail.com>
2024-11-14 09:01:16 -0500

    blog search bar complete, fully styled and responsive

--- src/app/(Routes)/blog/page.jsx
+++ src/app/(Routes)/blog/page.jsx
@@ -2,6 +2,7 @@ import "./blog.scss";
 import BlogList from "@/app/data/blogList";
 import BlogPost from "@/app/components/BlogPost";
 import { IoIosArrowForward } from "react-icons/io";
+import { FaSearch } from "react-icons/fa";
 
 const Blog = () => {
   return (
@@ -13,6 +14,14 @@ const Blog = () => {
           })}
         </div>
         <div className="blog-find-container">
+          <div className="blog-search-bar">
+            <div class="search">
+              <input type="text" class="searchTerm" placeholder="Search..." />
+              <button type="submit" class="searchButton">
+                <FaSearch />
+              </button>
+            </div>
+          </div>
           <h2 className="categories-title">Categories</h2>
           <div className="categories-drop-down">
             <span>
