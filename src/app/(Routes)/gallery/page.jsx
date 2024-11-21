8aff60322164c8b50bd4c027ff95b9333e8fecd0
Noah <noahglennmaldonado@gmail.com>
2024-11-21 01:16:16 -0500

    created general styling and elements for gallery page

--- src/app/(Routes)/gallery/page.jsx
+++ src/app/(Routes)/gallery/page.jsx
@@ -1,13 +1,33 @@
+import "./gallery.scss";
+import GalleryList from "@/app/data/galleryList";
+import Image from "next/image";
+
 const Gallery = () => {
   return (
     <div className="gallery-page-container">
-      <h2>Our Gallery</h2>
-      <p>
-        Here are a few Photos from the members at our gym showcasing different
-        classes, equipment, and Intensity. Become a Patron today and you may see
-        yourselve among these photos someday!
-      </p>
-      <div className="gallery-container"></div>
+      <div className="gallery-text">
+        <h2>Our Gallery</h2>
+        <p>
+          Here are a few Photos from the members at our gym showcasing different
+          classes, equipment, and Intensity. Become a Patron today and you may
+          see yourselve among these photos someday!
+        </p>
+      </div>
+
+      <div className="gallery-container">
+        {GalleryList.map((galleryImg) => {
+          return (
+            <div key={galleryImg} className="gallery-post">
+              <Image
+                className="gallery-post-img"
+                alt={galleryImg}
+                src={galleryImg}
+              />
+            </div>
+          );
+        })}
+        {/* <Image src={GalleryList[0]} /> */}
+      </div>
     </div>
   );
 };
--- src/app/data/galleryList.jsx
+++ src/app/data/galleryList.jsx
@@ -1,3 +1,17 @@
-const GalleryList = [];
+import workoutGirl1 from "@/app/assets/gallery/sushil-ghimire-DC5akQJyH4I-unsplash.jpg";
+import overHeadPress from "@/app/assets/gallery/cyan-cooper-zFmorSIqLHw-unsplash.jpg";
+import weightLift1 from "@/app/assets/gallery/alora-griffiths-LOnMc8Rp1Qs-unsplash.jpg";
+import weightLift2 from "@/app/assets/gallery/logan-weaver-lgnwvr-IXXkrUO2qw0-unsplash.jpg";
+import weightLift3 from "@/app/assets/gallery/john-fornander-TAZoUmDqzXk-unsplash.jpg";
+import weightLift4 from "@/app/assets/gallery/dollar-gill-PcBbrjiShAk-unsplash.jpg";
+
+const GalleryList = [
+  workoutGirl1,
+  overHeadPress,
+  weightLift1,
+  weightLift2,
+  weightLift3,
+  weightLift4,
+];
 
 export default GalleryList;
