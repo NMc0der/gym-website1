cae76a0cb495d2f011374756b6afa00040b2f334
Noah <noahglennmaldonado@gmail.com>
2024-11-22 06:32:54 -0500

    gallery page fully responsive and added more phote entries to gallery list

--- src/app/data/galleryList.jsx
+++ src/app/data/galleryList.jsx
@@ -4,6 +4,9 @@ import weightLift1 from "@/app/assets/gallery/alora-griffiths-LOnMc8Rp1Qs-unspla
 import weightLift2 from "@/app/assets/gallery/logan-weaver-lgnwvr-IXXkrUO2qw0-unsplash.jpg";
 import weightLift3 from "@/app/assets/gallery/john-fornander-TAZoUmDqzXk-unsplash.jpg";
 import weightLift4 from "@/app/assets/gallery/dollar-gill-PcBbrjiShAk-unsplash.jpg";
+import workoutGirl2 from "@/app/assets/gallery/samuel-girven-4shR8qNZ1E0-unsplash.jpg";
+import weightLift5 from "@/app/assets/gallery/john-fornander-dMWL8V7L8G4-unsplash.jpg";
+import workoutGirl3 from "@/app/assets/gallery/trust-tru-katsande-Pu7BOaYlYGY-unsplash.jpg";
 
 const GalleryList = [
   workoutGirl1,
@@ -12,6 +15,9 @@ const GalleryList = [
   weightLift2,
   weightLift3,
   weightLift4,
+  workoutGirl2,
+  weightLift5,
+  workoutGirl3,
 ];
 
 export default GalleryList;
