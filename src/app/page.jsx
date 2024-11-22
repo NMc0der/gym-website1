339a6803ba28d5b5874483e72a83d7125537c008
Noah <noahglennmaldonado@gmail.com>
2024-11-22 08:20:11 -0500

    started creating hero section for the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,25 @@
 import Image from "next/image";
+import heroImg from "./assets/daniel-apodaca-WdoQio6HPVA-unsplash.jpg";
 import "./styles/global.scss";
-
+import "@/app/home.scss";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="title">Hello Sass</div>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+    <div>
+      <div className="home-hero-section">
+        <div className="home-hero-text">
+          <h2>MAKE YOUR BODY</h2>
+          <h3>FIT & PERFECT</h3>
+        </div>
+        <Image className="home-hero-img" src={heroImg} />
+      </div>
     </div>
   );
+}
+
+{
+  /* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="title">Hello Ss</div>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+    </div> */
 }
