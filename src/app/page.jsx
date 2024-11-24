ec3497da615498a4756b8c4df3c7c7a94eb00177
Noah <noahglennmaldonado@gmail.com>
2024-11-24 00:10:57 -0500

    hero cards styling mostly complete

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,10 @@ import heroImg from "./assets/daniel-apodaca-WdoQio6HPVA-unsplash.jpg";
 import "./styles/global.scss";
 import "@/app/home.scss";
 import Button from "./components/button";
+import { IoIosTimer } from "react-icons/io";
+import { GiWeightLiftingUp } from "react-icons/gi";
+import { VscChecklist } from "react-icons/vsc";
+
 export default function Home() {
   return (
     <div>
@@ -17,6 +21,41 @@ export default function Home() {
         </div>
 
         <Image className="home-hero-img" src={heroImg} />
+        <div className="home-hero-cards">
+          <div className="home-hero-card">
+            <div className="home-hero-progress-icon">
+              <IoIosTimer />
+            </div>
+
+            <h2>PROGRESSION</h2>
+            <p>
+              Our team of experts will work with you to create a customized plan
+              that helps you achieve success one step at a time.
+            </p>
+          </div>
+          <div className="home-hero-card">
+            <div className="home-hero-progress-icon">
+              <GiWeightLiftingUp />
+            </div>
+
+            <h2>WORKOUT</h2>
+            <p>
+              With a variety of workouts to choose from, you'll have everything
+              you need to get into the best shape of your life.
+            </p>
+          </div>
+          <div className="home-hero-card">
+            <div className="home-hero-progress-icon">
+              <VscChecklist />
+            </div>
+
+            <h2>NUTRITION</h2>
+            <p>
+              Our team will work with you to create a personalized meal plan
+              that helps you reach your specific health goals.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
