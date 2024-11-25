8a62dd21a1168b1d36a6d1850bca5a92052ddee3
Noah <noahglennmaldonado@gmail.com>
2024-11-25 07:10:22 -0500

    virtual tour section complete for mobile devices

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,10 +6,13 @@ import Button from "./components/button";
 import { IoIosTimer } from "react-icons/io";
 import { GiWeightLiftingUp } from "react-icons/gi";
 import { VscChecklist } from "react-icons/vsc";
+import { GiStrong } from "react-icons/gi";
+import { CgGym } from "react-icons/cg";
+import { IoNutrition } from "react-icons/io5";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home-container">
       <div className="home-hero-section">
         <div className="home-hero-text">
           <h2>MAKE YOUR BODY</h2>
@@ -55,6 +58,37 @@ export default function Home() {
               that helps you reach your specific health goals.
             </p>
           </div>
+        </div>
+      </div>
+      <div className="virtual-tour-container">
+        <h2>Take a Virtual Tour!</h2>
+        <p>
+          Take a virtual tour with with one of our professional trainers, Rob
+          Daniels. View are various types of equipment, exercieses and meet with
+          some of our trainers.{" "}
+        </p>
+        <div className="virtual-tour-icons">
+          <div className="virtual-tour-icon">
+            <GiStrong />
+            <h3>
+              PROFESSIONAL <br /> TRAINERS
+            </h3>
+          </div>
+          <div className="virtual-tour-icon">
+            <CgGym />
+            <h3>
+              MODERN <br /> EQUIPMENT
+            </h3>
+          </div>
+          <div className="virtual-tour-icon">
+            <IoNutrition />
+            <h3>
+              DIET <br /> PLANNING
+            </h3>
+          </div>
+        </div>
+        <div className="tour-button-container">
+          <Button text={"TAKE A TOUR"} />
         </div>
       </div>
     </div>
