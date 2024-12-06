acba77dc99739b67abd07bf6832e9ff9d12544a5
Noah <noahglennmaldonado@gmail.com>
2024-12-06 00:25:20 -0500

    coaches section general layout and styling complete

--- src/app/components/homeSection/homeCoach.jsx
+++ src/app/components/homeSection/homeCoach.jsx
@@ -1,6 +1,8 @@
 import Coach from "./coach";
 // import weights1 from "@/app/assets/coach/sam-sabourin-PiFA6HIAfBA-unsplash.jpg";
 import weights1 from "@/app/assets/coach/samuel-girven-Ppua0I4VjqE-unsplash.jpg";
+import crossfit1 from "@/app/assets/coach/joey-nicotra-du1DgQJvlTg-unsplash.jpg";
+import yoga1 from "@/app/assets/coach/julia-rekamie-Z72YujnOrlY-unsplash.jpg";
 const HomeCoach = () => {
   return (
     <div className="home-coach-container">
@@ -17,6 +19,12 @@ const HomeCoach = () => {
           nameText={"John Lewis"}
           img={weights1}
         />
+        <Coach
+          coachType={"Crossfit Trainer"}
+          nameText={"Janice Martin"}
+          img={crossfit1}
+        />
+        <Coach coachType={"Yoga Trainer"} nameText={"Katy Smith"} img={yoga1} />
       </div>
     </div>
   );
