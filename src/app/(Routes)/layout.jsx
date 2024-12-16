16611add0f60690f319d57715188f8d1084ca954
Noah <noahglennmaldonado@gmail.com>
2024-12-16 02:48:41 -0500

    setup layout section to recognize and display the current page

--- src/app/(Routes)/layout.jsx
+++ src/app/(Routes)/layout.jsx
@@ -1,14 +1,25 @@
+"use client";
+
 // import "../styles/global.scss";
 import "../styles/breakpoint.scss";
 // import heroImg from "../assets/scott-webb-Vn39uEkX00s-unsplash.jpg";
 import heroImg2 from "../assets/sven-mieke-optBC2FxCfc-unsplash.jpg";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const Layout = ({ children }) => {
+  const pathname = usePathname();
+
+  const pathTitle = pathname
+    .slice(1)
+    .replace(/([a-z])([A-Z])/g, "$1 $2")
+    .toUpperCase();
+
   return (
     <>
       <div className="pages-hero-section">
-        <h2>About</h2>
+        <h2>{pathTitle}</h2>
+        {/* <p>Current pathname: {pathTitle}</p> */}
         <Image className="hero-img" src={heroImg2} alt="" />
       </div>
       {children}
